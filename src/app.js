facd17431a884ed0c8ab28a8e40bd17c8812440c
Laura Suez <laura.suez@outlook.com>
2024-05-19 19:04:38 -0300

    agrego api/ a rutas

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/products", productsRouter);
-app.use("/carts", cartsRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
 
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
